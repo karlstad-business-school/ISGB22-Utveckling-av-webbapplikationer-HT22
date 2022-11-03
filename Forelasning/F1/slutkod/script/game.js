b6f109b099aca264f3e0e156b7c0a5617fea82a5
peteratkau <59958667+peteratkau@users.noreply.github.com>
2022-11-03 12:30:06 +0100

    Add files via upload

---
+++ Forelasning/F1/slutkod/script/game.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class GameData  {
+
+    imgRefs = [
+        'https://openclipart.org/download/282127/Die1.svg',
+        'https://openclipart.org/download/282128/Die2.svg',
+        'https://openclipart.org/download/282129/Die3.svg',
+        'https://openclipart.org/download/282130/Die4.svg',
+        'https://openclipart.org/download/282131/Die5.svg',
+        'https://openclipart.org/download/282132/Die6.svg'
+    ];
+
+    createImgElements() {
+
+    }
+
+    removeImgElements() {
+        
+    }
+
+};
+
+window.addEventListener('load', function() {
+
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+
+});
