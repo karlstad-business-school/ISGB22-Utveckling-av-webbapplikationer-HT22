4f373d776d1684ea7bdc81d06acf8bd46622947d
peteratkau <59958667+peteratkau@users.noreply.github.com>
2022-11-07 15:42:56 +0100

    Uppdaterat W1
    Uppdaterat W1

--- Workshop/W1/slutkod/script/game.js
+++ Workshop/W1/slutkod/script/game.js
@@ -10,12 +10,19 @@ class GameData {
     GameBegin = 0;
     GameEnd = 0;
 
+    //För att skapa spöken med kortare interval efter en viss tid
+    milliseconds = 1000;
+
+    //För att skapde spöken skall bli mindre och mindre
+    imgWidth = 400;
+    imgHeight = 211;
+
     calculateGhostLeft() {
-        return Math.round( Math.random() * screen.width ) + 1;
+        return Math.round( Math.random() * (screen.availWidth - this.imgWidth)) + 1;
     }
 
     calculateGhostTop() {
-        return Math.round( Math.random() * screen.height ) + 1;
+        return Math.round( Math.random() * (screen.availHeight - this.imgHeight)) + 1;
     }
 
     removeGhosts(imgRefs) {
@@ -34,6 +41,83 @@ class GameData {
 
 }
 
+//Vår kod!
+let oGameData = new GameData();
+
+window.addEventListener('load', function() {
+
+    console.log( screen.height, screen.availHeight);
+    document.addEventListener('keydown', function(e) {
+
+        /*B = Begin*/
+        if(e.key === 'b' || e.key === 'B') {
+
+            if(oGameData.timerId !== 0) {
+                clearInterval(oGameData.timerId);
+            }
+
+            oGameData.GameBegin = Date.now();            
+
+            oGameData.timerId = setInterval(function() {
+
+                let imgRef = document.createElement('img');
+                let top = oGameData.calculateGhostTop();
+                let left = oGameData.calculateGhostLeft();
+
+                imgRef.setAttribute('alt', oGameData.imgAlt);
+                imgRef.setAttribute('src', oGameData.imgSrc);
+                imgRef.style.left = left + 'px';
+                imgRef.style.top = top + 'px';     
+
+                //För att storleken på bilden skall bli mindre och mindre
+                imgRef.style.width = oGameData.imgWidth + 'px';
+                imgRef.style.height = oGameData.imgHeight + 'px';
+
+                document.querySelector('#gameField').appendChild(imgRef);
+
+                oGameData.antalSpoken++;
+                imgRef.addEventListener('mouseover', function() {
+                    this.remove();
+                    oGameData.antalKlickadSpoken++;
+                });
+
+                //Antal skapade spöken styr hur snabbt ett nytt spöke skall göras synligt
+                if(oGameData.antalSpoken % 10 === 0) {
+                    oGameData.milliseconds -= 100;
+                    oGameData.imgHeight -= 25;
+                    oGameData.imgWidth -= 50;
+
+                    //Game Over
+                    if( oGameData.milliseconds === 0) {
+                        //Simulera en tangentbordstryckning på e!
+                    }
+
+                    console.log( oGameData.milliseconds);
+                }
+
+            }, oGameData.milliseconds);
+
+        }
+
+        /*E = Slut*/
+        if(e.key ==='e' || e.key === 'E') {
+            
+            oGameData.GameEnd = Date.now();
+
+            console.log( oGameData.GameEnd - oGameData.GameBegin);
+
+            clearInterval(oGameData.timerId);
+
+            alert('Antalet spöken var ' + oGameData.antalSpoken + ' och antalet fångade spöken var ' + oGameData.antalKlickadSpoken);
+
+            oGameData.prepareForNewGame();
+            oGameData.removeGhosts(document.querySelectorAll('#gameField img'));
+
+        }
+
+    });
+
+});
 
 
 
