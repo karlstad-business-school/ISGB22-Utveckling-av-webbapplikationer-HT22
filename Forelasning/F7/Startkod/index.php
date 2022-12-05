7c514db057ffbc6e3706b20c339e6bce33ecb045
alexvest <alexander.vestin@kau.se>
2022-12-05 12:06:53 +0100

    F7 startkod + slutkod

--- Forelasning/F7/Startkod/index.php
+++ Forelasning/F7/Startkod/index.php
@@ -8,14 +8,74 @@
         <main>
             <?php 
             
-                //Kodexempel med utskrift
+                //Kodexempel med utskrift och variabler
+                $hej = "Hej";
+                echo("<p>Hej</p>");
+                echo("<p>" . $hej . "</p>");
+                echo "Testar";
 
-                //Kodexempel med variabler
+                $num = 123;
+                $str = "String";
+
+                echo("<p>" . gettype($num) . " | " . gettype($str) . "</p>");
+
+                $int = 234;
+                $boolean = true;
+                $str = "512";
+                $str2 = "abc123";
+                settype($int, "string");
+                settype($boolean, "string");
+                settype($str, "integer");
+                settype($str2, "integer");
+
+                echo("<p>" . $int . "</p>");
+                echo("<p>" . $boolean . "</p>");
+                echo("<p>" . $str . "</p>");
+                echo("<p>" . $str2 . "</p>");
 
                 //Kodexempel med selektion
 
+                $klienthp = 3.5;
+                $serverhp = 3.5;
+                $helkurs = 7.0;
+
+                if($klienthp + $serverhp === $helkurs){
+                    echo("Du är klar med kursen");
+                }else{
+                    echo("Du har nog något kvar!");
+                }
+
+
                 //Kodexempel med iteration
 
+                for($i = 1; $i < 7; $i++){
+                    echo(
+                        "<h" . $i . "> 
+                        Header: ". $i . "
+                        </h" . $i . ">");
+                }
+
+
+                echo("<script>alert('hej hej');</script>");
+
+                echo("<p style=\"background-color: red; \">Hej hej</p>");
+
+
+
+
+                $array = array(
+                    "ett" => 1,
+                    "två" => 2,
+                    "tre" => 3
+                );
+
+                printR($array);
+                
+                function printR($vektor){
+                    echo("<pre>");
+                    print_r($vektor);
+                    echo("</pre>");
+                }
             ?>
         </main>
     </body>
---
+++ Forelasning/F7/slutkod/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <title>PHP F7</title>
+</head>
+<body>
+    <main>
+        <?php 
+        
+            //Kodexempel med utskrift och variabler
+            $kurskod = "ISGB02";
+            echo($kurskod);
+
+            $kursnamn = "Webbutveckling";
+            $hp = 7.5;
+
+            echo("<p>" . $kurskod . "</p>");
+            echo("<p>$kurskod, $kursnamn, $hp");
+
+            echo("<p>" . gettype($kurskod) . " " . gettype($hp) . "</p>");
+
+            echo("<script>alert('hahaha');</script>");
+
+            //För gettype() och settype() se 
+            //https://www.php.net/manual/en/function.gettype.php
+            //https://www.php.net/manual/en/function.settype.php
+
+
+            $int = 234;
+            $boolean = true;
+            $str = "512";
+            $str2 = "abc123";
+
+            settype($int, "string");
+            settype($boolean, "string");
+            settype($str, "integer");
+            settype($str2, "integer");
+
+            echo("<p>" . $int . "</p>");
+            echo("<p>" . $boolean . "</p>");
+            echo("<p>" . $str . "</p>");
+            echo("<p>" . $str2 . "</p>");
+
+
+
+            //Kodexempel med selektion
+            $klienthp = 3.5;
+            $serverhp = 4.0;
+            $helkurs = 7.5;
+            if($klienthp + $serverhp === $helkurs) {
+                echo("<p>Grattis du har klarat hela kursen!</p>");
+            } else {
+                echo("<p>Nja du har nog ngt kvar</p>");
+            }
+
+            //Kodexempel med iteration
+            for($i = 1; $i < 7;  $i++) {
+                echo("<h$i>Detta är rubriknivå $i</h$i>"). PHP_EOL;
+                //echo("<script>alert('inte igen...');</script>");
+            }
+
+            echo("<div style='color: pink;'>Detta är en</div>");
+            echo("<div style=\"color: lightblue;\">Detta är en text!</div>");
+          
+        ?>
+    </main>
+</body>
+</html>
