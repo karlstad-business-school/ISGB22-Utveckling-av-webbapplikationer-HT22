0abc3dbf26e5933562a995929c25c5393465b19e
peteratkau <59958667+peteratkau@users.noreply.github.com>
2022-12-06 14:50:20 +0100

    Uppdaterat W8
    Uppdaterat W8

--- Workshop/W8/F8_startkod.php
+++ Workshop/W8/F8_startkod.php
@@ -8,16 +8,55 @@
         <main>
         
         <?php
-            $dataSourceName = "mysql:" . "host=localhost;" . "dbname=car;" . "charset=utf8";
-            $userName = "isgb22";
-            $passWord = "isgb22";
-            $dbhsOptions = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            );
-            
-            $dbh = new PDO($dataSourceName, $userName, $passWord, $dbhsOptions);
+            try {
+                $dataSourceName = "mysql:" . "host=localhost;" . "dbname=car;" . "charset=utf8";
+                $userName = "isgb22";
+                $passWord = "isgb22";
+                $dbhsOptions = array(
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                );
+                
+                $dbh = new PDO($dataSourceName, $userName, $passWord, $dbhsOptions);
 
+                //echo("Japp!");
+                $sql = "SELECT * FROM cars;";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+
+                //Antalet rader...
+                echo("<p>" . $stmt->rowCount() . "</p>");
+                //Dumpa "tabellen"...
+                //$rs = $stmt->fetchAll();
+                /*
+                echo("<pre>");
+                print_r($rs);
+                echo("</pre>");
+                */
+
+                //Iterera igenom "tabellen" (tabell -> rad -> kolumn)
+                while($row = $stmt->fetch()) {
+
+                    //print_r($row);
+
+                    echo("<div>");
+                    
+                    echo("<p>Fabrikat: " . $row["fabrikat"] . "</p>");
+                    echo("<p>Modell: " . $row["modell"] . "</p>");
+
+                    echo("</div>" . PHP_EOL);
+
+                }
+
+
+            }catch(PDOException $e) {
+                echo("<div>" . $e->getMessage() . "</div>");
+            } finally{
+
+                echo("<script>console.log('städar');</script>");
+                $stmt = null;
+                $dbh = null;
+            }
             
         ?>
         
